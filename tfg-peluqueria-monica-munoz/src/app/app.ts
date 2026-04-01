a49d4c18a0fb833d910d9690061945d0a99da406
Monica-pxl <monica.munozdelatorre@cieep.com>
2026-04-01 12:44:16 +0200

    cambios en diseño de dashboard admin

--- tfg-peluqueria-monica-munoz/src/app/app.ts
+++ tfg-peluqueria-monica-munoz/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from "./cliente/components/navbar-component/navbar-component";
 import { FooterComponent } from "./cliente/components/footer-component/footer-component";
 import { AlertComponent } from "./shared/components/alert/alert.component";
@@ -12,6 +14,25 @@ import { AvisoPrivacidadComponent } from "./shared/components/aviso-privacidad/a
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('tfg-peluqueria-monica-munoz');
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.router.events
+        .pipe(filter(e => e instanceof NavigationEnd))
+        .subscribe(() => {
+          setTimeout(() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0;
+          }, 0);
+        });
+    }
+  }
 }
